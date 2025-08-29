e959a044a63d2895ae7a584143222992173f5511
marcin <marcin.konowalczyk@canonical.com>
2025-08-29 21:16:53 +0100

    ci!: apply pr labels script

--- .github/scripts/forward-port-missing/apply_pr_labels.sh
+++ .github/scripts/forward-port-missing/apply_pr_labels.sh
@@ -1,4 +1,31 @@
-cat results.json | jq -c '.[]' | while read -r pr; do
+#!/bin/bash
+# usage: cat results.json | ./apply_pr_labels.sh --dry-run/-n
+
+dry_run=false
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -n|--dry-run)
+            dry_run=true
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+_maybe_dry_run() {
+    if [ "$dry_run" = true ]; then
+        # squash multiple spaces into one
+        echo "> $(echo "$1" | tr -s ' ')"
+    else
+        eval "$1"
+    fi
+}
+
+jq -c '.[]' | while read -r pr; do
     number=$(echo "$pr" | jq -r '.number')
     title=$(echo "$pr" | jq -r '.title')
     url=$(echo "$pr" | jq -r '.url')
@@ -8,18 +35,17 @@ cat results.json | jq -c '.[]' | while read -r pr; do
     label=$(echo "$pr" | jq -r '.label')
 
     echo "PR #$number: $title"
-    echo "  URL: $url"
-    echo "  base: $base"
-    echo "  head: $head"
+    echo "  $url"
+    echo "  $head -> $base"
     echo "  forward_ported: $forward_ported"
-    echo "  label: $label"
+    echo "  has label: $label"
 
     if [ "$forward_ported" = false ] && [ "$label" = false ]; then
         echo "  Adding the 'forward port missing' label."
-        gh pr edit "$number" --add-label "forward port missing"
+        _maybe_dry_run "gh pr edit $number --add-label \"forward port missing\""
     elif [ "$forward_ported" = true ] && [ "$label" = true ]; then
         echo "  Removing the 'forward port missing' label."
-        gh pr edit "$number" --remove-label "forward port missing"
+        _maybe_dry_run "gh pr edit $number --remove-label \"forward port missing\""
     else
         echo "  No label changes needed."
     fi
